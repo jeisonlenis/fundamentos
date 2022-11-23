f63d6bb9d1014c5dba1bd461d05f931456b51ef5
jeisonlenis <jlenism@gmail.com>
2022-11-23 04:57:53 -0500

    first commit

---
+++ src/main/java/com/fundamentosplatzi/springboot/appfundamentos/AppfundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.appfundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfundamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/springboot/appfundamentos/AppfundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.appfundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppfundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
